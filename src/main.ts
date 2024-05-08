d0b9425cb16214971787e25501bd4ff21481e4fd
Davy De Groote (personal) <davy.degr@gmail.com>
2024-05-08 07:49:31 +0200

    Enable global validation pipes with  verbose logging for development

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  // Enable CORS
   app.enableCors()
+
+  // Enable a validation pipe with debug messages only on development
+  const isDev = process.env.NODE_ENV === 'development'
+  const validationPipe = new ValidationPipe({
+    enableDebugMessages: isDev,
+    disableErrorMessages: !isDev,
+  })
+  app.useGlobalPipes(validationPipe)
+
+  // Start the application
   await app.listen(3001)
 }
 bootstrap()
